30918edf0b3b03ac5aed287651f50d2014d8a74a
potxolate <potxolate@hotmail.com>
2025-01-13 17:34:29 +0100

    fix links styles

--- resources/views/links/edit.blade.php
+++ resources/views/links/edit.blade.php
@@ -3,37 +3,56 @@
 
 	@section('content')
 
-	<h1 class="text-center m-2">Edit a Link</h1>
-	<div class="w-50 text-center mx-auto">
-	<!-- if there are creation errors, they will show here -->
-	{{ HTML::ul($errors->all() )}}
+	<div class="container my-4">
+    <!-- Título -->
+    <h1 class="text-center mb-4">Edit a Link</h1>
 
-	{{ Form::open(array('url' => route('links.update', $link->id))) }}
-    @csrf
-    @method('PUT')        
-		<div class="form-group p-4">
-			{{ Form::label('product', 'Product') }}
-			{{ Form::text('search', $link->product->name, array('class' => 'typeahead form-control', 'id' => 'search')) }}
-			{{ Form::hidden('product_id', $link->product_id, array('class' => 'typeahead form-control', 'id' => 'product_id')) }}
-		</div>
+    <!-- Mostrar errores de validación -->
+    @if ($errors->any())
+        <div class="alert alert-danger w-50 mx-auto">
+            <ul class="mb-0">
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
 
-		<div class="form-group p-4">
-			{{ Form::label('url', 'URL') }}
-			{{ Form::url('url', $link->url,  array('class' => 'form-control')) }}
-            <p></p>
-		</div>
-		
-		<div class="form-group p-4">
-			{{ Form::label('price', 'Price') }}
-			{{ Form::text('price', $link->price,  array('class' => 'form-control')) }}
-            <p></p>
-		</div>
+    <!-- Formulario -->
+    <div class="card w-50 mx-auto shadow-sm">
+        <div class="card-body">
+            {{ Form::open(['url' => route('links.update', $link->id), 'method' => 'PUT']) }}
+            @csrf
+            
+            <!-- Campo Producto -->
+            <div class="form-group mb-4">
+                {{ Form::label('product', 'Product', ['class' => 'form-label fw-bold']) }}
+                {{ Form::text('search', $link->product->name, ['class' => 'form-control typeahead', 'id' => 'search', 'placeholder' => 'Search product...']) }}
+                {{ Form::hidden('product_id', $link->product_id, ['id' => 'product_id']) }}
+            </div>
 
-		{{ Form::submit('Update the Link!', array('class' => 'btn btn-primary')) }}
+            <!-- Campo URL -->
+            <div class="form-group mb-4">
+                {{ Form::label('url', 'URL', ['class' => 'form-label fw-bold']) }}
+                {{ Form::url('url', $link->url, ['class' => 'form-control', 'placeholder' => 'Enter the URL...']) }}
+            </div>
 
-	{{ Form::close() }}
+            <!-- Campo Precio -->
+            <div class="form-group mb-4">
+                {{ Form::label('price', 'Price', ['class' => 'form-label fw-bold']) }}
+                {{ Form::text('price', $link->price, ['class' => 'form-control', 'placeholder' => 'Enter the price...']) }}
+            </div>
 
-	</div>
+            <!-- Botón de envío -->
+            <div class="text-center">
+                {{ Form::submit('Update the Link!', ['class' => 'btn btn-primary px-4']) }}
+            </div>
+            
+            {{ Form::close() }}
+        </div>
+    </div>
+</div>
+
 	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-3-typeahead/4.0.1/bootstrap3-typeahead.min.js"></script>
 	<script type="text/javascript">
--- resources/views/links/index.blade.php
+++ resources/views/links/index.blade.php
@@ -1,66 +1,78 @@
 @extends('layouts.app')
 
-	@section('content')
-	@if (session()->has('message'))
+@section('content')
+@if (session()->has('message'))
 
-	<p class="alert alert-success">{{ session('message') }}</p>
+<p class="alert alert-success">{{ session('message') }}</p>
 
 
-	@endif
-		<h1 class="text-center my-2">All the links</h1>
-		<div class="row">
-			<div class="d-flex justify-content-center">
-				<form action="{{ route('links.search') }}" method="GET" >
-					<input type="text" name="search" placeholder="Search">
-					<button type="submit" class="btn btn-small btn-info m-2 p-1">Search</button>
-				</form>						
-			</div>			
+@endif
+<div class="container my-4">
+	<!-- Título -->
+	<h1 class="text-center my-3">All the Links</h1>
+
+	<!-- Formulario de búsqueda -->
+	<div class="row justify-content-center mb-4">
+		<div class="col-md-6">
+			<form action="{{ route('links.search') }}" method="GET" class="input-group">
+				<input type="text" name="search" placeholder="Search" class="form-control" />
+				<button type="submit" class="btn btn-info">Search</button>
+			</form>
 		</div>
-		
-		<div class="d-flex flex-row-reverse m-3"><a href="{{ URL::to('links/create') }}">Create a Link</a></div>
-		<!-- will be used to show any messages -->
-		@if (Session::has('message'))
-		<div class="alert alert-info">{{ Session::get('message') }}</div>
-		@endif
-		
-		<table class="table table-striped table-bordered mx-auto w-75">
-			<thead>
+	</div>
+
+	<!-- Botón para crear un enlace -->
+	<div class="text-end mb-3">
+		<a href="{{ URL::to('links/create') }}" class="btn btn-success">Create a Link</a>
+	</div>
+
+	<!-- Mensaje de sesión -->
+	@if (Session::has('message'))
+	<div class="alert alert-info text-center">{{ Session::get('message') }}</div>
+	@endif
+
+	<!-- Tabla de enlaces -->
+	<div class="table-responsive">
+		<table class="table table-striped table-bordered text-center">
+			<thead class="table-dark">
 				<tr>
-					<td class="text-center">ID</td>
-					<td class="text-center">Product</td>
-					<td class="text-center">Price</td>
-					<td class="text-center">Actions</td>
+					<th>ID</th>
+					<th>Product</th>
+					<th>Price</th>
+					<th>Actions</th>
 				</tr>
 			</thead>
 			<tbody>
 				@foreach($links as $key => $value)
 				<tr>
-					<td class="text-center">{{ $value->id }}</td>
+					<td>{{ $value->id }}</td>
 					<td class="py-2">
 						{{ $value->product->name }}<br />
-						<a href="{{ $value->url }}">{{ $value->getDominioAttribute() }}</a>
+						<a href="{{ $value->url }}" class="text-decoration-none">{{ $value->getDominioAttribute() }}</a>
 					</td>
-					<td class="text-center">{{ $value->price }}</td>
-					<!-- we will also add show, edit, and delete buttons -->
+					<td>{{ $value->price }} €</td>
 					<td>
-						<div class="d-flex justify-content-end">
-							<!-- edit this nerd (uses the edit method found at GET /links/{id}/edit -->
-							<a class="btn btn-small btn-info m-2" href="{{ URL::to('links/' . $value->id . '/edit') }}">Edit</a>
-							
-							<!-- delete the nerd (uses the destroy method DESTROY /links/{id} -->
-							<!-- we will add this later since its a little more complicated than the first two buttons -->
-							{{ Form::open(array('url' => 'links/' . $value->id, 'class' => 'pull-left')) }}
-							{{ Form::hidden('_method', 'DELETE') }}
-							{{ Form::submit('Delete', array('class' => 'btn btn-danger m-2 ')) }}
-							{{ Form::close() }}							
+						<div class="d-flex justify-content-center">
+							<!-- Botón de editar -->
+							<a href="{{ URL::to('links/' . $value->id . '/edit') }}" class="btn btn-info btn-sm mx-1">Edit</a>
+							<!-- Botón de eliminar -->
+							{{ Form::open(['url' => 'links/' . $value->id, 'method' => 'DELETE', 'class' => 'd-inline']) }}
+							{{ Form::submit('Delete', ['class' => 'btn btn-danger btn-sm mx-1']) }}
+							{{ Form::close() }}
 						</div>
 					</td>
 				</tr>
 				@endforeach
 			</tbody>
 		</table>
-		@isset($links)
-			<div class="d-flex my-2 justify-content-center">{{ $links->links() }}</div>
-		@endisset		
-	@endsection
+	</div>
 
+	<!-- Paginación -->
+	@isset($links)
+	<div class="d-flex justify-content-center my-4">
+		{{ $links->links() }}
+	</div>
+	@endisset
+</div>
+
+@endsection
